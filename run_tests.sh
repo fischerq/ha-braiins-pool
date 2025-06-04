3f546530dd1202fdb10aa6b03df0e7746f1c85e5
quirin.niedernhuber <quirin.niedernhuber@gmail.com>
2025-06-04 07:58:27 +0000

    fix

--- run_tests.sh
+++ run_tests.sh
@@ -5,8 +5,4 @@
 # Remember to activate your Python virtual environment if you are using one:
 # source venv/bin/activate
 
-# Option 1: Run tests using Python's built-in unittest module
-python -m unittest discover tests
-
-# Option 2: Run tests using pytest (uncomment the line below if you have pytest installed and prefer it)
-# pytest tests
+pytest tests

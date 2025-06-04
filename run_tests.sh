9946243002136621078b2832bcc9fbe85969fec6
quirin.niedernhuber <quirin.niedernhuber@gmail.com>
2025-06-04 08:25:01 +0000

    python path

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,5 @@
 
 # This script is used to run the unit tests for the Braiins Pool Home Assistant integration.
 
-# Remember to activate your Python virtual environment if you are using one:
-# source venv/bin/activate
-
+PYTHONPATH=.
 pytest tests/
